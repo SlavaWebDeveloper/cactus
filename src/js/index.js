4d5ee278ab37e2c896d00a7a5ad40f2aa799de2f
Slava Sidorov <sidorov-vv3@mail.ru>
2024-03-27 12:30:27 +0700

    "Update HTML structure, add new sections, and modify SCSS for header and nav"
    This diff includes changes to the HTML structure of the home page, with the addition of new sections for 'new', 'shop', 'care', and 'contact'. The header and nav sections in the SCSS have also been modified, with changes to the header's blur effect and the nav's position and styling.

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,12 @@
 import mobileNav from './modules/mobile-nav.js';
 mobileNav();
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const blurHeader = () =>{
+  const header = document.getElementById('header')
+  // Add a class if the bottom offset is greater than 50 of the viewport
+  window.scrollY >= 50 ? header.classList.add('blur-header') 
+                     : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
